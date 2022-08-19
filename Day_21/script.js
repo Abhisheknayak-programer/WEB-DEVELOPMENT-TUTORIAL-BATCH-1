f0bfb2df8ddd6f1f77a57341b74616d082a7c951
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2022-08-19 06:13:12 +0530

    JavaScript Basics Covered (let, const, console.log())

---
+++ Day_21/script.js
@@ -0,0 +1,30 @@
+let a = 25;
+let b = 26;
+let c = 27;
+
+console.log(a + b + c);
+console.log(a * b * c);
+console.log(250 / 5);
+console.log(1000 - 500);
+console.log("Akhil is not doing homework!");
+console.log("Akhil have not done Homework once again!!");
+
+a = 50;
+a = 80;
+console.log(a);
+
+const name = "Abhishek";
+console.log(name);
+// name = "Akshara";
+// console.log(name);
+
+let name2 = "Brindha";
+console.log(name2);
+name2 = "Akshara";
+console.log(name2);
+
+// HOMEWORK
+// 1. Declare 5 Variables and store some values inside that
+// 2. What is the value of 500 * 1234 * 98765 print that to console
+// 3. Declare five constant variable and print sum of this five variables
+// 4. Declare a name and then change the name three times and then print the lastly changed name
