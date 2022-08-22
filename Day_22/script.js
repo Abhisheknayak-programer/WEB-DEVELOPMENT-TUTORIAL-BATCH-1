ffcf8dca42e61c5e81096691d273643e7137660b
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2022-08-22 06:12:02 +0530

    Conditional Statement taught (If else ladder and if else)

---
+++ Day_22/script.js
@@ -0,0 +1,55 @@
+// IF AND ELSE
+let akhilAge = 21;
+
+if (akhilAge >= 21) {
+  console.log("I can drive");
+}
+
+let akhsharaMark = 70;
+
+if (akhsharaMark <= 70) {
+  console.log("try harder to get more mark 👍");
+}
+
+let BrindhaSalary = 10000;
+let AksharaSalary = 1000;
+
+AksharaSalary = BrindhaSalary;
+BrindhaSalary = AksharaSalary;
+
+if (BrindhaSalary === AksharaSalary) {
+  console.log("💸");
+}
+
+console.log("Brindha done the homework!");
+
+let akhilSalary = 20;
+if (akhilSalary < 200) {
+  console.log("Very Less Salary!");
+} else {
+  console.log(" 👍 ");
+}
+
+let money = 26;
+if (money > 20) {
+  console.log("I will give treat");
+} else if (money > 25) {
+  console.log("👍 I will give treat twice");
+} else {
+  console.log("No Money No Treat!");
+}
+
+let schoolTime = -10;
+if (schoolTime >= 5) {
+  console.log("Correct Time");
+} else if (schoolTime <= 3) {
+  console.log("Wrong Time");
+} else if (schoolTime === 4) {
+  console.log("Medium Time");
+} else {
+  console.log("TIME TIME TIME");
+}
+
+// HOMEWORK
+// 1. Check Whether a number is a positive number or negative number or a zero using if else ladder.
+// 2. Try to make some of the if else and if else ladder conditions and show that to me in next class. [10 Different Examples]
